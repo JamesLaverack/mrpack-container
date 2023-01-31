b5c8b100c144ad314533b61fd8986f81ed3602b3
James Laverack <james@laverack.dev>
2023-01-31 23:27:40 +0000

    Add additional debug output

--- src/packfile.rs
+++ src/packfile.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 // Based on Modrinth documentation https://docs.modrinth.com/docs/modpacks/format_definition/
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Index {
     pub format_version: u64,
@@ -14,7 +14,7 @@ pub struct Index {
     pub dependencies: Dependencies,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct Dependencies {
     pub minecraft: String,
@@ -23,7 +23,7 @@ pub struct Dependencies {
     pub quilt_loader: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct File {
     pub path: String,
@@ -33,7 +33,7 @@ pub struct File {
     pub file_size: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Hashes {
     #[serde(with = "hex")]
     pub sha1: [u8; 20],
@@ -41,13 +41,13 @@ pub struct Hashes {
     pub sha512: [u8; 64],
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Env {
     pub client: EnvType,
     pub server: EnvType,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
 pub enum EnvType {
     Required,
